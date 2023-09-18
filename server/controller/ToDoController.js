767f8055c09b636c71b1894ee246cab9c53e1d3d
sejal99 <sejal8974@gmail.com>
2023-09-18 23:45:09 +0530

    newest commit

--- server/controller/ToDoController.js
+++ server/controller/ToDoController.js
@@ -1,6 +1,16 @@
 const ToDoModels = require("../models/ToDoModels");
 
 module.exports.getToDos = async (req, res) => {
-    const toDos = await ToDoModel.find();
+    const toDos = await ToDoModels.find();
     res.send(toDos);
+  };
+
+  module.exports.saveToDo =  (req, res) => {
+    const {toDo}=req.body;
+    ToDoModels.create({toDo})
+    .then((data)=>{
+      console.log("saved successfully");
+      res.status(201).send(data);
+    })
+    .catch((err)=>console.log(err));
   };
--- server/routes/ToDoRoutes.js
+++ server/routes/ToDoRoutes.js
@@ -1,11 +1,13 @@
 const Router=require("express");
-const { getToDos } = require("../controller/ToDoController");
+const { getToDos, saveToDo } = require("../controller/ToDoController");
 
 const router=Router();
 //grt all the todos
 
 router.get("/get",getToDos);
-// router.post("/save",saveToDo);
+
+ 
+router.post("/save",saveToDo);
 
 
 module.exports = router;

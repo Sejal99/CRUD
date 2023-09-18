e4afa63442c9762b17c7608f5dc46469b7d6ef50
sejal99 <sejal8974@gmail.com>
2023-09-18 21:52:06 +0530

    new commit

--- server/routes/ToDoRoutes.js
+++ server/routes/ToDoRoutes.js
@@ -2,6 +2,7 @@ const Router=require("express");
 const { getToDos } = require("../controller/ToDoController");
 
 const router=Router();
+//grt all the todos
 
 router.get("/get",getToDos);
 // router.post("/save",saveToDo);

98b0588c6115c3a7e3418bab125554a83d82ade1
sejal99 <sejal8974@gmail.com>
2023-09-19 22:42:55 +0530

    neww commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+  
+<h1 style={{textAlign:"center"}}>Todo App</h1>
+
+  <div style={{textAlign:"center"}} className='input_holder'>
+    <input type="text" placeholder='Add a todo'/>
+    <button>Add</button>
+  </div>
+    </div>
+  
+  )
+}
+
+export default App

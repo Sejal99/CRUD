f9d4c9981e8f1170be9d33f9ba191090bf485c8d
sejal99 <sejal8974@gmail.com>
2023-10-01 19:37:45 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -1,23 +1,21 @@
+
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-const routes = require("./routes/ToDoRoutes");
-
+const dotenv=require("dotenv");
+dotenv.config()
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
 const cors = require("cors");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
-  
- app.use("/api", routes);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" })
+ .then(()=>console.log('database connected'))
+ .catch((err)=>console.log(err))
 
-app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Server running on port ' +  port));
